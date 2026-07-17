0114e7daa6250ac1d9841c63693c59b97f070538
Steeven <steeveric5@gmail.com>
2026-07-17 20:12:19 +0300

    feat(database): Add table cours

---
+++ app/Database/Migrations/02_17-07-2026-migration.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys = ON;
+CREATE TABLE cours (
+    id_cours INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre_cours VARCHAR(150) NOT NULL,
+    volume_horaire INTEGER NOT NULL,
+    coefficient DECIMAL(4, 2) NOT NULL DEFAULT 1.00,
+    id_enseignant INTEGER NOT NULL,
+    id_filiere INTEGER NOT NULL,
+    FOREIGN KEY (id_enseignant) REFERENCES enseignant(id_enseignant) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_filiere) REFERENCES filiere(id_filiere) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+CREATE INDEX idx_cours_enseignant ON cours(id_enseignant);
+CREATE INDEX idx_cours_filiere ON cours(id_filiere);
