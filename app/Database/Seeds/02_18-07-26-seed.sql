1f74fc228ebbdbf7717bb0b9bf499d38f60d3765
Steeven <steeveric5@gmail.com>
2026-07-18 15:45:53 +0300

    fix(Controller): Remove temp controller

---
+++ app/Database/Seeds/02_18-07-26-seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO cours (titre_cours, volume_horaire, coefficient, id_enseignant, id_filiere) VALUES
+('Programmation Web CI4',       45, 3.00, 1, 1),
+('Bases de Données SQLite',     30, 2.00, 2, 1),
+('Réseaux TCP/IP',               50, 3.00, 3, 2),
+('Administration Systèmes',      35, 2.00, 1, 2);
